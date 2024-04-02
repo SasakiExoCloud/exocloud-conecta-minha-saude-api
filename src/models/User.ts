2de5390f8d566950b11c0c35ca51eff045954953
sasaki-exo-cloud <igor.sasaki@exocloudsolutions.com.br>
2024-04-02 12:49:04 -0300

    feat(models user): implement user logic model

---
+++ src/models/User.ts
@@ -0,0 +1,18 @@
+export interface IUser {
+  address: string
+  addressNumber: string
+  city: string
+  createdAt: string
+  crmCode: string
+  crmStatus: string
+  document: string
+  email: string
+  medicalSpecialty: string
+  name: string
+  password: string
+  phone: string
+  postalCode: string
+  state: string
+  updatedAt: string
+  userId: string
+}
