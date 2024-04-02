3fb7c9c1c7af1f43f5ba992d2351528e5956c617
sasaki-exo-cloud <igor.sasaki@exocloudsolutions.com.br>
2024-04-02 12:49:30 -0300

    feat(models patient): implement patient logic model

---
+++ src/models/Patient.ts
@@ -0,0 +1,19 @@
+export interface IPatient {
+  address: string
+  addressNumber: string
+  birthDate: Date
+  city: string
+  createdAt: string
+  description?: string
+  document: string
+  email: string
+  // height: number
+  name: string
+  password: string
+  patientId: string
+  phone: string
+  postalCode: string
+  state: string
+  updatedAt: string
+  // weight: number
+}
